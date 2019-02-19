2573d528fc8a894a6f993a01ba2366ef3beba993
Maxim <maksa988ua@gmail.com>
2019-02-20 01:52:28 +0200

    Supports PHP 5.6

--- src/Exceptions/InvalidSearchOrder.php
+++ src/Exceptions/InvalidSearchOrder.php
@@ -12,7 +12,7 @@ class InvalidSearchOrder extends \Exception
      * @param int $code
      * @param Throwable|null $previous
      */
-    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, $previous = null)
     {
         if(empty($message))
             $message = "FreeKassa config: searchOrder callback not set";
